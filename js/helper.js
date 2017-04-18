1fd4e369f77a207d301ec9a2ebb966ed32cbcffd
Octavian Rotari <octavianrotari@gmail.com>
2017-04-18 22:58:29 +0100

    change const to var in helper

--- js/helper.js
+++ js/helper.js
@@ -1,55 +1,54 @@
-const HTMLimageSidebar = '<img src="%data%" class="main-photo" alt="">';
+var HTMLimageSidebar = '<img src="%data%" class="main-photo" alt="">';
 
-const HTMLheader = '<div>%data%</div>';
-const HTMLname = '<h1>%data%</h1><hr>';
-const HTMLrole = '<h2>%data%</h2>';
-const HTMLwelcomeMessage = '<p>%data%</p>';
+var HTMLheader = '<div>%data%</div>';
+var HTMLname = '<h1>%data%</h1><hr>';
+var HTMLrole = '<h2>%data%</h2>';
+var HTMLwelcomeMessage = '<p>%data%</p>';
 
-const HTMLcontact = '<div class="contact"></div>';
-const HTMLiconContacts = '<i><img src="img/%data%.svg" alt=""></i>';
-const HTMLcontactInfo = '<p id="%contactType%">%data%</p>';
+var HTMLcontact = '<div class="contact"></div>';
+var HTMLiconContacts = '<i><img src="img/%data%.svg" alt=""></i>';
+var HTMLcontactInfo = '<p id="%contactType%">%data%</p>';
 
 // main icons
-const HTMLiconMain = '<i><img src="%data%" alt=""> </i>';
+var HTMLiconMain = '<i><img src="%data%" alt=""> </i>';
 
 // content menu
-const HTMLcontentHeader = '<ul id="menu"></ul>';
-const HTMLcontentMenu = '<li data-value="%data-value%">%data%</li>';
+var HTMLcontentHeader = '<ul id="menu"></ul>';
+var HTMLcontentMenu = '<li data-value="%data-value%">%data%</li>';
 
 // jobs
-const HTMLjobEmployer = '<h4 id="employer">%data%</h4>';
-const HTMLjobTitle = '<h5 id="job-title">%data%</h5>';
-//const HTMLjobLocation = '<h5 id="job-location">%data%</h5>';
-const HTMLjobDate = '<h5 id="job-dates">%data%</h5>';
-const HTMLjobDescription = '<p id="job-description">%data%</p>';
+var HTMLjobEmployer = '<h4 id="employer">%data%</h4>';
+var HTMLjobTitle = '<h5 id="job-title">%data%</h5>';
+var HTMLjobDate = '<h5 id="job-dates">%data%</h5>';
+var HTMLjobDescription = '<p id="job-description">%data%</p>';
 
 // dropdown menu content header
-const HTMLdropdownMenu = '<ul id="dropdown"></ul>';
-const HTMLdropdownLiMenu = '<li data-value="%data-value%" class="dropdown-li">%data%</li>';
+var HTMLdropdownMenu = '<ul id="dropdown"></ul>';
+var HTMLdropdownLiMenu = '<li data-value="%data-value%" class="dropdown-li">%data%</li>';
 
 // projects
 
-const HTMLprojectTitle = '<h4 id="project-title"><a target="_blank" href="%path%">%data%</a></h4>';
-const HTMLprojectLanguage = '<h5 id="project-language">%data%</h5>';
-const HTMLprojectFrameworks = '<h5 id="project-frameworks">%data%</h5>';
-const HTMLprojectDescription = '<p id="project-description">%data%</p>';
-const HTMLprojectImage = '<img src="%data%" alt="" class="project">';
-const HTMLprojectGithub = '<i><a target="_blank" href="%github%"><img src="img/github.svg" alt=""></a></i>';
+var HTMLprojectTitle = '<h4 id="project-title"><a target="_blank" href="%path%">%data%</a></h4>';
+var HTMLprojectLanguage = '<h5 id="project-language">%data%</h5>';
+var HTMLprojectFrameworks = '<h5 id="project-frameworks">%data%</h5>';
+var HTMLprojectDescription = '<p id="project-description">%data%</p>';
+var HTMLprojectImage = '<img src="%data%" alt="" class="project">';
+var HTMLprojectGithub = '<i><a target="_blank" href="%github%"><img src="img/github.svg" alt=""></a></i>';
 
 // education
-const HTMLeducationName = '<h4 id="education-name"><a target="_blank" href="%path%">%data%</a></h4>';
-const HTMLeducationLocation = '<h5 id="education-location">%data%</h5>';
-const HTMLeducationType = '<h5 id="education-type">%data%</h5>';
-const HTMLeducationDate = '<h5 id="education-dates">%data%</h5>';
-const HTMLeducationDescription = '<p id="education-description">%data%</p>';
-const HTMLeducationGrade = '<h5 id="education-grade">Grade: %data%%</h5>';
-const HTMLeducationCertificate = '<img class="certificate" src="%data%" alt="">';
+var HTMLeducationName = '<h4 id="education-name"><a target="_blank" href="%path%">%data%</a></h4>';
+var HTMLeducationLocation = '<h5 id="education-location">%data%</h5>';
+var HTMLeducationType = '<h5 id="education-type">%data%</h5>';
+var HTMLeducationDate = '<h5 id="education-dates">%data%</h5>';
+var HTMLeducationDescription = '<p id="education-description">%data%</p>';
+var HTMLeducationGrade = '<h5 id="education-grade">Grade: %data%%</h5>';
+var HTMLeducationCertificate = '<img class="certificate" src="%data%" alt="">';
 
-const HTMLdivText = '<div class="text"></div>';
-const HTMLdivMultimedia = '<div class="multimedia"></div>';
+var HTMLdivText = '<div class="text"></div>';
+var HTMLdivMultimedia = '<div class="multimedia"></div>';
 // div for animation
 
-const HTMLdivAnimation = '<div class="animation"></div>';
-const googleMap = '<div id="map" class="map-big"></div>';
-const HTMLplaces = '<h2 class="places">Places I</br><span>worked</span></br>and</br><span>lived in</span></h2>';
-const googleMapSmall = '<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCEcdmaS5zsj8QJBFnvNTvjkbeDliYHXz4&q=place_id:%data%" allowfullscreen></iframe>';
+var HTMLdivAnimation = '<div class="animation"></div>';
+var googleMap = '<div id="map" class="map-big"></div>';
+var HTMLplaces = '<h2 class="places">Places I</br><span>worked</span></br>and</br><span>lived in</span></h2>';
+var googleMapSmall = '<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCEcdmaS5zsj8QJBFnvNTvjkbeDliYHXz4&q=place_id:%data%" allowfullscreen></iframe>';
